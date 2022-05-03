14a575f41f002940788edaa61fd5980ae4d1bc74
Tan90degrees <72683500+Tan90degrees@users.noreply.github.com>
2022-05-03 21:23:19 +0800

    ADD : support to socks v4

--- main/main.go
+++ main/main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"os"
 	"proxy/httpp"
+	"proxy/socketp"
 )
 
 const DEFAULTPORT string = "10086"
@@ -12,8 +13,12 @@ const DEFAULTPORT string = "10086"
 func main() {
 	ps := flag.Bool("ps", false, "Start proxy server.")
 	ts := flag.Bool("ts", false, "Start tunnel server.")
+	socks := flag.Bool("d", false, "")
 	port := flag.String("p", DEFAULTPORT, "Set port.")
 	flag.Parse()
+	if *socks {
+		socketp.Server()
+	}
 	if *ps == *ts {
 		fmt.Println("Can only open one server.")
 		os.Exit(0)
